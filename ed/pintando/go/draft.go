99687a9be40a2621669bc3890a77280c87bbd573
jeniferrcarolinaa <jeniferrcarolinaa@gmail.com>
2026-03-25 13:50:06 -0300

    atividade 1 part 1

---
+++ ed/pintando/go/draft.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a, b, c float64
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	var s, area, valor float64
+	s = (a + b + c) / 2
+	valor = s * (s - a) * (s - b) * (s - c)
+	area = math.Sqrt(valor)
+	fmt.Printf("%v.2f", area)
+
+}

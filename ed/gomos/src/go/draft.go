d2b21f5e1736b5c8766aa7e774a62f5b94c7a231
Jenifer Carolina <jeniferrcarolinaa@gmail.com>
2026-05-22 14:36:24 -0300

    gomos

---
+++ ed/gomos/src/go/draft.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var Q int
+	var D string
+	fmt.Scan(&Q, &D)
+	//ler posições x e y
+	var x, y int
+	for i := 0; i < Q; i++ {
+		fmt.Scan(&x, &y)
+		//a depender da direção D, calcular a nova posição
+		switch D {
+		case "L":
+			x--
+		case "R":
+			x++
+		case "U":
+			y++
+		case "D":
+			y--
+		}
+
+		fmt.Println(x, y)
+	}
+}

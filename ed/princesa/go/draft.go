5416ee1bf64f77e226e858cde0f90133ac693d96
Jenifer Carolina <jeniferrcarolinaa@gmail.com>
2026-05-22 18:53:01 -0300

    princesa

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+func imprimir(pessoas []int, pos int) {
+	//imprime as filas das rodadas indicando quem tem a espada
+	fmt.Printf("[ ")
+
+	// preencher o vetor de pessoas
+	for i := 0; i < len(pessoas); i++ {
+		pessoas[i] = i + 1 // pessoa 1 no i 0, pessoa 2 no i 1, etc
+		//quem está com a espada?
+		if pos == i { //se a posição da pessoa for igual ao índice, ela tem a espada
+			fmt.Printf("%d>", pessoas[i])
+		} else { //caso n tenha a espada
+			fmt.Printf("%d", pessoas[i])
+		}
+
+		//adicionar espaço entre os números
+
+		if i < len(pessoas) {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Printf("]\n")
+}
+
 func main() {
 	var N, E int
 	fmt.Scan(&N, &E)
@@ -17,29 +40,20 @@ func main() {
 	var pessoas = make([]int, N) // este é o slice de pessoas (tipo int) com tamanho/capacidade N
 	pos := E - 1
 
-	fmt.Printf("[ ")
-
-	// preencher o vetor de pessoas
-	for i := 0; i < N; i++ {
-		pessoas[i] = i + 1 // pessoa 1 no i 0, pessoa 2 no i 2, etc
-		//quem está com a espada?
-		if pos == i { //se a posição da pessoa for igual ao índice, ela tem a espada
-			fmt.Printf("%d>", pessoas[i])
-		} else { //caso n tenha a espada
-			fmt.Printf("%d", pessoas[i])
-		}
-
-		//adicionar espaço entre os números
-
-		if i < N {
-			fmt.Print(" ")
-		}
+	//enquanto houver mais de uma pessoa viva, o processo continua
+	for len(pessoas) > 1 {
+		imprimir(pessoas, pos)
+		morto := (pos + 1) % len(pessoas)                       // pessoas restantes
+		pessoas = append(pessoas[:morto], pessoas[morto+1:]...) //append junta os dois slices
+		pos = morto % len(pessoas)
 	}
+
+	imprimir(pessoas, pos)
 
 	//quem morre é o elemento na posição depois da pessoa com a espada
 	//morto := pos + 1
 	//sabendo que a fila é circular, se o morto = N, volta pro início
-	morto := (pos + 1) % len(pessoas) // pessoas restantes
+	//morto := (pos + 1) % len(pessoas) // pessoas restantes
 	//retirar o morto da lista de pessoas
 	//manter tudo antes do morto e juntar com tudo depois do morto
 	//pegar o slice de pessoas antes do morto
@@ -48,12 +62,10 @@ func main() {
 	//dMorto := pessoas[morto+1:]
 	//juntar os dois slices
 	//pessoas = append(aMorto, dMorto...)
-	pessoas = append(pessoas[:morto], pessoas[morto+1:]...) //append junta os dois slices
+
 	//o ... é pra desagrupar o slice dMorto, adicionando elemento por elemento
 	//para quem é transferida a espada?
 	//para a pessoa que agora ocupa a posição que era do morto
-	pos = morto % len(pessoas)
-
-	fmt.Printf("]\n")
-
+	//pos = morto % len(pessoas)
+	//precisa criar uma função para mostrar todo o processo, de cada rodada
 }

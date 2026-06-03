5363d021f9a0af8fa6800caa927b4f15f50b1ffb
Jenifer Carolina <jeniferrcarolinaa@gmail.com>
2026-06-03 00:03:58 -0300

    vetbuild

---
+++ ed/vetbuild/src/go/main.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Vector struct {
+	data     []int
+	size     int
+	capacity int
+}
+
+func NewVector(capacity int) *Vector {
+	//função para criar um vetor
+	return &Vector{
+		data: make([]int, capacity), // nunca use len(data) ou cap(data) ou qq método do go de manipulação de array
+		//é assim que se cria um vetor
+		size:     0,
+		capacity: capacity,
+	}
+}
+
+// criar função status
+// retorns o tamanho e a capacidade do vetor
+func (v *Vector) Status() string {
+	return fmt.Sprintf("size:%d capacity:%d", v.size, v.capacity)
+}
+
+// criar função String pra imprimir o vetor
+func (v *Vector) String() string { // função se chama string, recebe um ponteiroi Vector e devolve tipo string
+
+	s := "["
+
+	for i := 0; i < v.size; i++ {
+
+		if i < v.size-1 {
+			s += ", "
+		}
+		s += fmt.Sprintf("%d", v.data[i])
+	}
+
+	s += "]"
+	return s
+}
+
+// fun Reserve para aumentar a capacidade do vetor
+func (v *Vector) Reserve(newCapacity int) {
+	newData := make([]int, newCapacity)
+	for i := 0; i < v.size; i++ {
+		newData[i] = v.data[i]
+	}
+	v.data = newData
+	v.capacity = newCapacity
+}
+
+// pushBack para adicionar um elemento ao final do vetor
+func (v *Vector) PushBack(value int) {
+	//o vetor tem capacidade pra adicionar o elemento?
+	//se tiver, adicionar o elemento e aumenta o size
+	//não tendo, aumentar capacity com reserve e depois adicionar o elemento
+	//fazer verificação
+	if v.size < v.capacity {
+		//adicionar o elento no vetor v, na posição size
+		//value é o elemento a ser adicionado
+		v.data[v.size] = value
+		//incrementar o size
+		v.size++
+	}
+	//não tendo capacidade, aumentar a capacidade do vetor
+	v.Reserve(v.capacity * 2)
+	//agr posso adicionar o elemento
+	v.data[v.size] = value
+	v.size++
+	//aff, vou ter que criar outra função pra aumentar a capacidade do vetor
+}
+
+func Join(slice []int, sep string) string {
+	if len(slice) == 0 {
+		return ""
+	}
+	var result strings.Builder
+	fmt.Fprintf(&result, "%d", slice[0])
+	for _, value := range slice[1:] {
+		fmt.Fprintf(&result, "%s%d", sep, value)
+	}
+	return result.String()
+}
+
+func main() {
+	var line, cmd string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	v := NewVector(0)
+	for {
+		fmt.Print("$")
+		if !scanner.Scan() {
+			break
+		}
+		line = scanner.Text()
+		fmt.Println(line)
+		parts := strings.Fields(line)
+		if len(parts) == 0 {
+			continue
+		}
+		cmd = parts[0]
+
+		switch cmd {
+		case "end":
+			return
+		case "init":
+			value, _ := strconv.Atoi(parts[1])
+			v = NewVector(value)
+		case "push":
+			for _, part := range parts[1:] {
+				value, _ := strconv.Atoi(part)
+				v.PushBack(value)
+			}
+		case "show":
+			fmt.Println(v)
+		case "status":
+			fmt.Println(v.Status())
+		case "pop":
+			// err := v.PopBack()
+			// if err != nil {
+			// 	fmt.Println(err)
+			// }
+		case "insert":
+			// index, _ := strconv.Atoi(parts[1])
+			// value, _ := strconv.Atoi(parts[2])
+			// err := v.Insert(index, value)
+			// if err != nil {
+			// 	fmt.Println(err)
+			// }
+		case "erase":
+			// index, _ := strconv.Atoi(parts[1])
+			// err := v.Erase(index)
+			// if err != nil {
+			// 	fmt.Println(err)
+			// }
+		case "indexOf":
+			// value, _ := strconv.Atoi(parts[1])
+			// index := v.IndexOf(value)
+			// fmt.Println(index)
+		case "contains":
+			// value, _ := strconv.Atoi(parts[1])
+			// if v.Contains(value) {
+			// 	fmt.Println("true")
+			// } else {
+			// 	fmt.Println("false")
+			// }
+		case "clear":
+			// v.Clear()
+		case "capacity":
+			// fmt.Println(v.Capacity())
+		case "get":
+			// index, _ := strconv.Atoi(parts[1])
+			// value, err := v.At(index)
+			// if err != nil {
+			// 	fmt.Println(err)
+			// } else {
+			// 	fmt.Println(value)
+			// }
+		case "set":
+			// index, _ := strconv.Atoi(parts[1])
+			// value, _ := strconv.Atoi(parts[2])
+			// err := v.Set(index, value)
+			// if err != nil {
+			// 	fmt.Println(err)
+			// }
+			//
+		case "reserve":
+			newCapacity, _ := strconv.Atoi(parts[1])
+			v.Reserve(newCapacity)
+		case "slice":
+			// start, _ := strconv.Atoi(parts[1])
+			// end, _ := strconv.Atoi(parts[2])
+			// slice := v.Slice(start, end)
+			// fmt.Println(slice)
+		default:
+			fmt.Println("fail: comando invalido")
+		}
+	}
+}
